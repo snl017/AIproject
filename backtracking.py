e9b32787ce561cab9859b11a15a5b796224a201e
Emma <jarthurcs@localhost>
2014-04-23 19:52:03 -0700

    code written to put kids in roommate pairs! all that's left to generate roommates is to write the constraints and to order the domains

--- backtracking.py
+++ backtracking.py
@@ -1,24 +1,48 @@
+import getdata
+
+#FAKE AC-3
+
+#checks the constraints
+#returns whether the pair can be roommates
+#studentFeaturesArray is a dictionary
+#x,y are indices of students
+def pairAllowed(x,y,studentFeaturesArray):
+	#THIS IS WHERE WE PUT CONSTRAINTS
+	#e.g. if studentFeaturesArray[x]<3 & studentFeaturesArray[y]>6...
+	#if we reach a constraint for which it doesn't work, return false.
+	#otherwise return true.
+	return True
+
+# Gets the (arc-consistent) domains for each of the students
+# student features array is a dictionary
+def arcConsistentDomains(studentFeaturesArray):
+	#arcs that have already been checked: tuples (i,j) and (j,i)
+	domains = {}
+	for i in studentFeaturesArray.keys():
+		#the domain of this student
+		iDomain = []
+		#for each other student, check if they could be roommates
+		for j in studentFeaturesArray.keys():
+			if (i!=j):
+				if pairAllowed(i,j,studentFeaturesArray):
+					iDomain.append(j)
+		#add the domain
+		domains[i] = iDomain
+	return domains
 
 
 
-# RUN AC-3
-
-
-# function CSP-BACKTRACKING(assignment) returns a solution or failure 
 # THIS RUNS FAKE BACKTRACKING ON THE STUDENTS TO HAVE THEM PUT IN ROOMMATE PAIRS
-
-
-
 
 #finds the next student to assign
 #chooses the student with the smallest domain
-def heuristic(alreadyAssigned, domainsArray):
+def heuristic(alreadyAssigned, domains):
 	smallestDomain = float("inf")
 	toReturn = None
-	for i in range(len(domainsArray)):
+	for i in domains.keys():
 		if not (i in alreadyAssigned):
-			if smallestDomain>len(domainsArray[i]):
-				smallestDomain=len(domainsArray[i])
+			if smallestDomain>len(domains[i]):
+				smallestDomain=len(domains[i])
 				toReturn =i
 
 	return toReturn
@@ -31,19 +55,21 @@ def order(domain):
 
 #remove students that have been assigned 
 #if this assignment leaves someone with nothing in their domain, return NONE
-def domainRemove(student1,student2,domainsArray):
+def domainRemove(student1,student2,domains,alreadyAssigned):
 	#if domain remove works, return domainsArray.
-	for index in range(len(domainsArray)):
-		if student1 in domainsArray[index]:
-			domainsArray[index].remove(student1)
-		if student2 in domainsArray[index]:
-			domainsArray[index].remove(student2)
-		if len(domainsArray[index])==0:
-			#if this domain is empty now, return None
-			return None
+	for index in domains.keys():
+		#if the student already has a roommate, don't worry about changing its domain
+		if not (index in alreadyAssigned):
+			if student1 in domains[index]:
+				domains[index].remove(student1)
+			if student2 in domains[index]:
+				domains[index].remove(student2)
+			if len(domains[index])==0:
+				#if this domain is empty now, return None
+				return None
 
 	#return the satisfactory domainsArray with the roommate pair removed from all domains
-	return domainsArray
+	return domains
 	
 
 #param: assignment: dictionary of assignments
@@ -51,15 +77,17 @@ def domainRemove(student1,student2,domainsArray):
 #Recursive algorithm assigns roommate pairs
 #returns None if no assignment can be found (failure)
 
-def backtrackingPairs(assignment,domainsArray):
+def backtrackingPairs(assignment,domains):
 	#  if assignment complete return assignment 
-	if (len(assignment==len(domainsArray))):
+	if (len(assignment)==len(domains.keys())):
 		return assignment
 
 	#  X select unassigned variable 
-	nextStudent = heuristic(assignment.keys(),heuristic)
+	#USE HEURISTIC
+	nextStudent = heuristic(assignment.keys(),domains)
+
 	#  D select an ordering for the domain of X 
-	orderedDomainValues = order(domainsArray[nextStudent])
+	orderedDomainValues = order(domains[nextStudent])
 	
 	#  for each value in D 
 	for roommatePossibility in orderedDomainValues:
@@ -67,23 +95,58 @@ def backtrackingPairs(assignment,domainsArray):
 		assignment[nextStudent]=roommatePossibility
 		assignment[roommatePossibility]=nextStudent
 		#remove the roommates from everyone else's domain
-		newDomainsArray = domainRemove()
-		if newDomainsArray:
+		newDomains = domainRemove(nextStudent,roommatePossibility,domains,assignment.keys())
+		if newDomains:
 			#go recursively.
 			#  result CSP-BACKTRACKING(assignment) 
-			result = backtracking(assignment,newDomainsArray)
+			result = backtrackingPairs(assignment,newDomains)
 			if result: #if result isn't failure
 				return result
 
-	#if we have no roommate assignment that will work in our domain, return null (failure)
+	#if we have no roommate assignment that will work in our domain, return None (failure)
 	return None
 
 
-	#DO PREPROCESSING -> PEOPLE AS FEATURE ARRAYS 
-	#RUN AC-# ON PEOPLE
-	#RUN BACKTRACKING_PAIRS (above, not really backtracking) ON PEOPLE -> ROOMMATES
+
+#MAIN FUNCTION
+
+#get the student feature vectors as a dictionary
+#DO PREPROCESSING -> PEOPLE AS FEATURE ARRAYS 
+studentFeaturesArray=getdata.getStudentValues()
+studentDict = {}
+for i in range(len(studentFeaturesArray)):
+	studentDict[i]=studentFeaturesArray[i]
+
+
+#split them into male and female
+maleStudentsFeatures = {}
+femaleStudentsFeatures = {}
+for i in studentDict.keys():
+	if studentDict[i][0]=="M":
+		maleStudentsFeatures[i]=studentDict[i]
+	else:
+		femaleStudentsFeatures[i]=studentDict[i]
+
+#RUN AC-3 ON PEOPLE
+#create the array of arc-consistent domains (possible roommates) for each student
+domainsMale=arcConsistentDomains(maleStudentsFeatures)
+domainsFemale = arcConsistentDomains(femaleStudentsFeatures)
+
+#RUN BACKTRACKING_PAIRS ON PEOPLE -> ROOMMATES
+#get the set of roommate pairs
+malePairs= backtrackingPairs({}, domainsMale) #this is a dictionary of the roommate pairs!
+femalePairs = backtrackingPairs({}, domainsFemale)
+malePairs.update(femalePairs)
+pairs = malePairs
+print pairs
+print len(pairs)
+
+
 	
-	#RUN AC-# ON ROOMMATE PAIRS
+	
+	
+	
+	#RUN AC-3 ON ROOMMATE PAIRS
 	#RUN REAL BACKTRACKING ON ROOMMATE PAIRS AS WE ASSIGN TO SPONSOR GROUPS
 
 
--- getdata.py
+++ getdata.py
@@ -3,28 +3,29 @@
 import xlrd
 import os
 
+def getStudentValues():
+	#access the housing data, which is just on the desktop
+	book = xlrd.open_workbook("../../Desktop/Housing Data 2017.xls")
 
-#access the housing data, which is just on the desktop
-book = xlrd.open_workbook("../../Desktop/Housing Data 2017.xls")
+	#we only have 1 sheet, and only care about the first sheet
+	housingData = book.sheet_by_index(0)
 
-#we only have 1 sheet, and only care about the first sheet
-housingData = book.sheet_by_index(0)
-
-print "Cell D30 is", housingData.cell_value(rowx=29, colx=3)
+	#print "Cell D30 is", housingData.cell_value(rowx=29, colx=3)
 
 
-studentValues = []
-numStudent = 0
-#this gets us an array of arrays
-#each individual array stores the information provided for each student
-#the first entry is gender, followed by 8 numbers for the ranking questions
-for rowIndex in range(1, housingData.nrows):
-	indivValues = []
-	for colIndex in range(1, 10):
-		print (rowIndex, colIndex)
-		cellVal = housingData.cell_value(rowx=rowIndex, colx=colIndex)
-		indivValues.append(cellVal)
-	studentValues.append(indivValues)
-   	numStudent += 1
+	studentValues = []
+	numStudent = 0
+	#this gets us an array of arrays
+	#each individual array stores the information provided for each student
+	#the first entry is gender, followed by 8 numbers for the ranking questions
+	for rowIndex in range(1, housingData.nrows):
+		indivValues = []
+		for colIndex in range(1, 10):
+			#print (rowIndex, colIndex)
+			cellVal = housingData.cell_value(rowx=rowIndex, colx=colIndex)
+			indivValues.append(cellVal)
+		studentValues.append(indivValues)
+	   	numStudent += 1
 
-print studentValues
+	#print studentValues
+	return studentValues
