1b41432f26f843a002f6f4720d2cc2b2f22fd88a
sjundt <sajundt@gmail.com>
2014-05-03 14:59:25 -0700

    compute centroid is not working. what i have so far.

--- cluster.py
+++ cluster.py
@@ -34,11 +34,43 @@ def closestCentroid(studentX, avgPairFeatures, centroids) :
 	return distances.index(min(distances))
 
 #TODO: COMPUTE CENTROIDS!!!!!!!!!!!!
-def computeCentroids(clusters,K):
+def computeCentroids(clusters,K,avgPairFeatures):
 	centroids = [-1]*K
+	num_features = len(avgPairFeatures[0])
 	for i in range(len(clusters)) :
 		#i am going to have to average all the things in the cluster,
 		#but for now i'm just setting the centroid to be the first one
+
+		# #average all things in the cluster
+		# sums = numpy.array([0]*num_features) #length of number of elements
+		# averages = [0]*num_features
+
+		# #for each student, add their values.
+		# for studentX in clusters[i]:
+		# 	xFeatures = avgPairFeatures[studentX]
+		# 	sums = sums + numpy.array(xFeatures)
+		# for j in range(num_features):
+		# 	averages[j] = sums[j]
+
+		# centroids[i] = averages
+
+
+
+		# #average all things in the cluster.
+		# sumForCluster =[0]*len(clusters[i][0])
+		# #for each student in the cluster:
+		# for j in range(len(clusters[i])):
+		# 	#average the features
+		# 	for k in range(len(sumForCluster)):
+		# 		sumForCluster[k] += clusters[i][j] 
+		# averageForCluster = [0]*len(clusters[i][0])
+		# for k in range(len(averageForCluster):
+		# 	sumForCluster[k]= sumForCluster[k]/float(len(clusters[i]))
+
+
+
+
+
 		centroids[i] = clusters[i][0]
 	#check to make sure all went well
 	if -1 in centroids:
@@ -75,7 +107,7 @@ def kcluster(studentPairs, studentFeatures, K) :
 			clusters[j].append(student)
 		for kk in range(K) :
 			#recompute centroids
-			centroids = computeCentroids(clusters,K)
+			centroids = computeCentroids(clusters,K,avgPairFeatures)
 		i += 1
 
 	#return centroids? really? 
